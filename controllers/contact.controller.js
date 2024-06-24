f71d0cc26676d3e207d40cb328bc267112d564ad
Nate-Soul <nanithsukpong@gmail.com>
2024-06-24 21:22:21 +0100

    feat: add services to controller for more modular structure for handling request

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,21 +1,58 @@
+import { 
+    createContactService,
+    getContactsService,
+    getContactService,
+    editContactService,
+    deleteContactService
+} from "../services/contact.services.js";
+
+
 //create new contact logic goes here
 export const createContact = async (req, res) => {
+    try {
+        const createdContact = await createContactService(req);
+        res.status(201).json(createdContact);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 //fetch all contacts logic goes here
 export const getContacts = async (req, res) => {
-
+    try {
+        const allContacts = await getContactsService(req);
+        res.status(200).json(allContacts);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 //fetch single contact by ID logic goes here
 export const getContact = async (req, res) => {
-
+    try {
+        const contact = await getContactService(req);
+        res.status(200).json(contact);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 //edit single contact by ID logic goes here
 export const editContact = async (req, res) => {
+    try {
+        const editedContact = await editContactService(req);
+        res.status(200).json(editedContact);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 //delete single contact by ID logic goes here
 export const deleteContact = async (req, res) => {
+    try {
+        const createdContact = await deleteContactService(req);
+        res.status(404).json(createdContact);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
---
+++ services/contact.services.js
@@ -0,0 +1,18 @@
+export const createContactService = async (req) => {
+};
+
+export const getContactsService = async (req) => {
+
+};
+
+export const getContactService = async (req) => {
+
+};
+
+export const editContactService = async (req) => {
+
+};
+
+export const deleteContactService = async (req) => {
+
+};
