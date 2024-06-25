06f2df52c29a35e64109a583a332e66ff2e72cd5
Nate-Soul <nanithsukpong@gmail.com>
2024-06-25 15:44:33 +0100

    completed logic for contact services and controllers

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,3 +1,4 @@
+import Contact from "../models/contact.model.js";
 import { 
     createContactService,
     getContactsService,
@@ -6,53 +7,86 @@ import {
     deleteContactService
 } from "../services/contact.services.js";
 
+const devMode = Boolean(process.env.DEBUG);
 
-//create new contact logic goes here
+//create new contact
 export const createContact = async (req, res) => {
     try {
         const createdContact = await createContactService(req);
         res.status(201).json(createdContact);
+        if (devMode === true) {
+            console.log(`${req.method}: ${res.statusCode} | New Contact Created!`);
+        }
     } catch (err) {
+        res.status(400);
         console.log(err.message);
     }
 };
 
-//fetch all contacts logic goes here
+//get all contacts
 export const getContacts = async (req, res) => {
     try {
-        const allContacts = await getContactsService(req);
+        const allContacts = await getContactsService();
         res.status(200).json(allContacts);
     } catch (err) {
+        res.status(400);
         console.log(err.message);
     }
 };
 
-//fetch single contact by ID logic goes here
+//get single contact by ID
 export const getContact = async (req, res) => {
     try {
         const contact = await getContactService(req);
-        res.status(200).json(contact);
+        if(contact) {
+            res.status(200).json(contact);
+        } else {
+            res.status(404);
+            throw new Error("Contact Not Found!");
+        }
     } catch (err) {
+        res.status(400);
         console.log(err.message);
     }
 };
 
-//edit single contact by ID logic goes here
+//edit single contact by ID
 export const editContact = async (req, res) => {
     try {
-        const editedContact = await editContactService(req);
-        res.status(200).json(editedContact);
+        const contact = await editContactService(req);
+        if (contact) {
+            //An alternative would be extracting all model fields and updating only changed params manually
+            //something in this regard below
+            //contact.name = req.
+            const updatedContact = await Contact.findByIdAndUpdate(
+                req.params.id, 
+                {$set: req.body}, 
+                {new: true}
+            );
+            res.status(200).json(updatedContact);
+        } else {
+            res.status(404);
+            throw new Error("Contact Not Found!");
+        }
     } catch (err) {
+        res.status(400);
         console.log(err.message);
     }
 };
 
-//delete single contact by ID logic goes here
+//delete single contact by ID
 export const deleteContact = async (req, res) => {
     try {
-        const createdContact = await deleteContactService(req);
-        res.status(404).json(createdContact);
+        const contact = await deleteContactService(req);
+        if (contact) {
+            await Contact.deleteOne({_id: contact._id});
+            res.status(204).json({"detail": contact.name + "'s credentials has been deleted"});
+        } else {
+            res.status(404);
+            throw new Error("Contact Not Found!");
+        }
     } catch (err) {
+        res.status(400);
         console.log(err.message);
     }
 };
--- services/contact.services.js
+++ services/contact.services.js
@@ -1,18 +1,38 @@
+import Contact from "../models/contact.model.js";
+
+const findContactByID = async (req) => {
+    const contactId = req.params.id;
+    const contact = await Contact.findById(contactId);
+    return contact;
+}
+
+//create new contact logic goes here
 export const createContactService = async (req) => {
+    //comments below are for an ideal case where i'm certain of the parameters to work with.
+    //extract parameters from req.body to ascertain you're getting the right fields.
+    const newContact = new Contact(req.body);
+    //check if contact already exist before saving to database.
+    const savedContact = await newContact.save();
+    return savedContact;
 };
 
-export const getContactsService = async (req) => {
-
+//fetch all contacts logic goes here
+export const getContactsService = async () => {
+    const contacts = await Contact.find();
+    return contacts;
 };
 
+//fetch single contact by ID logic goes here
 export const getContactService = async (req) => {
-
+    return findContactByID(req);
 };
 
+//edit single contact by ID logic goes here
 export const editContactService = async (req) => {
-
+    return findContactByID(req);
 };
 
+//delete single contact by ID logic goes here
 export const deleteContactService = async (req) => {
-
+    return findContactByID(req);
 };
